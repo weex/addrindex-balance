c0a1548aa60c9a2fc4890edeec9318b750093e2e
David Sterry <davids@exchb.com>
2017-09-06 17:03:59 -0700

    show input values

--- transaction.py
+++ transaction.py
@@ -3,30 +3,45 @@ import json
 from decimal import Decimal
 import daemon
 
+def get_output(txout):
+    output = {}
+    if 'addresses' not in txout['scriptPubKey']:
+        return None
+    for a in txout['scriptPubKey']['addresses']:
+        output['n'] = txout['n']
+        output['value'] = txout['value']
+        output['address'] = a
+    return output
+
+def get_outputs(transaction):
+    outputs = []
+    for txout in transaction['vout']:
+        out = get_output(txout)
+        if out:
+            outputs.append(out)
+    return outputs
+
 def get_info(txid):
     d = daemon.Daemon()
     raw = d.getrawtransaction(txid)
     transaction = d.decoderawtransaction(raw)
 
-    outputs = []
     total_value = Decimal(0,8)
 
-    for txout in transaction['vout']:
-        out = {}
-        if 'addresses' not in txout['scriptPubKey']:
-            # non-standard output
-            continue
-        for a in txout['scriptPubKey']['addresses']:
-            out['n'] = txout['n'] 
-            out['value'] = txout['value']
-            out['address'] = a
-        outputs.append(out)    
+    outputs = get_outputs(transaction)
 
     inputs = []
     for txin in transaction['vin']:
         inp = {}
         inp['txid'] = txin['txid'] 
         inp['vout'] = txin['vout']
+        transaction = d.decoderawtransaction(d.getrawtransaction(inp['txid']))
+        source_outputs = get_outputs(transaction)
+        if source_outputs:
+            for s in source_outputs:
+                print "comparing n {} to vout {}".format(s['n'], txin['vout'])
+                if s['n'] == txin['vout']:
+                    inp['amount'] = s['value']
         inputs.append(inp)
  
     stats = {    
